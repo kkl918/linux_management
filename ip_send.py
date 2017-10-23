ecec3e79617bedbbf9a8de4d638ff28f7aa9c913
kkl918 <kakinglin@gmail.com>
2017-10-24 03:58:03 +0800

    Create ip_send.py

---
+++ ip_send.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import smtplib
+from email.message import EmailMessage
+from subprocess import check_output
+
+ip = check_output(["hostname", "-I"]).decode()
+#print(ip)
+
+ifconfig = check_output(["ifconfig"]).decode()
+#print(ifconfig)
+
+TO = 'kakinglin@gmail.com'
+SUBJECT = 'About IP'
+TEXT = 'hostname -I:'+ ip + '\n' + ifconfig
+
+# Gmail Sign In
+gmail_sender = 'kakinglin@gmail.com'
+gmail_passwd = 'bpvfswzcogzdvtil'
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.ehlo()
+server.starttls()
+server.login(gmail_sender, gmail_passwd)
+
+BODY = '\r\n'.join(['To: %s' % TO,
+                    'From: %s' % gmail_sender,
+                    'Subject: %s' % SUBJECT,
+                    '', TEXT])
+
+try:
+    server.sendmail(gmail_sender, [TO], BODY)
+    print ('email sent')
+except:
+    print ('error sending mail')
+
+server.quit()
