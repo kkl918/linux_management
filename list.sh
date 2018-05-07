bd21df5a02beefdbd8e4a25db478e66f874c33b8
kkl918 <kakinglin@gmail.com>
2018-05-07 17:56:48 +0800

    Update list.sh

--- list.sh
+++ list.sh
@@ -2,3 +2,16 @@ sudo apt-get install vim -y
 sudo apt-get install python3-pip -y
 sudo apt-get install git -y
 sudo apt-get install python-matplotlib -y
+
+
+# gcc -------------------------------------
+
+pushd .
+cd ~
+wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
+tar -jxf gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
+exportline="export PATH=$HOME/gcc-arm-none-eabi-7-2017-q4-major/bin:\$PATH"
+if grep -Fxq "$exportline" ~/.profile; then echo nothing to do ; else echo $exportline >> ~/.profile; fi
+popd
+
+# end -------------------------------------
