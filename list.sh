0045ff2bf0b87573552d65400f70501efe7f4a19
kkl918 <kakinglin@gmail.com>
2018-05-08 10:24:14 +0800

    Update list.sh

--- list.sh
+++ list.sh
@@ -5,13 +5,11 @@ sudo apt-get install python-matplotlib -y
 sudo apt install python-pip -y
 
 #　Pymavlink
-
 sudo apt-get install libxml2-dev libxslt-dev python-dev
 sudo pip2 install -U future lxml
 sudo pip2 install -U pymavlink
 
 # gcc -------------------------------------
-
 pushd .
 cd ~
 wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2017q4/gcc-arm-none-eabi-7-2017-q4-major-linux.tar.bz2
