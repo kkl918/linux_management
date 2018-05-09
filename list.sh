c41b7dbf4833ea0bb34fe7bb1ad9c7e6339006e5
kkl918 <kakinglin@gmail.com>
2018-05-09 16:39:25 +0800

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,8 +1,10 @@
-sudo apt-get install vim -y
-sudo apt-get install python3-pip -y
-sudo apt-get install git -y
-sudo apt-get install python-matplotlib -y
+sudo apt install vim -y
+sudo apt install python3-pip -y
+sudo apt install git -y
+sudo apt install python-matplotlib -y
 sudo apt install python-pip -y
+sudo apt-get install manpages-pl manpages-fr-extra -y
+
 pip3 install -r ./basic.txt
 git clone https://github.com/ArduPilot/ardupilot.git
 
