444351efe14b3eb70f8e7f9e4d88cf399b54e754
ChiaChing Lin <kakinglin@gmail.com>
2018-09-11 15:10:05 +0800

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,17 +1,17 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential
-sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libgtk2.0-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install python-dev python3-dev  // python
-sudo apt-get install python-pip python3-pip
+sudo apt-get install build-essential -y
+sudo apt-get install cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev -y
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
+sudo apt-get install libxvidcore-dev libx264-dev -y
+sudo apt-get install libgtk2.0-dev -y
+sudo apt-get install libatlas-base-dev gfortran -y
+sudo apt-get install python-dev python3-dev -y
+sudo apt-get install python-pip python3-pip -y
 pip install numpy
 pip3 install numpy
-sudo apt-get install git
+sudo apt-get install git  -y
 git clone https://github.com/opencv/opencv.git
 git clone https://github.com/opencv/opencv_contrib.git
 cd opencv
